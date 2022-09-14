3d3e1259222672c4009e64fb5254e53d45a1c422
umum29 <kaichu.liu@hotmail.com>
2022-09-14 15:45:42 -0700

    Create DataContext.cs

---
+++ DataContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+
+    }
+}
